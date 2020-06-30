8b48c0efca5cc8f51b26b0e2b1ad6926f4904a69
RakshithRAcharya <rakshithacharya1030@gmail.com>
2020-06-30 20:12:41 +0530

    Updates

---
+++ precache-manifest.5fabf13a7233f734c7b7527198cc3d17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc7fc92a771306cd6783c825e8a2660",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "743dd6d023bf18297349",
+    "url": "/covid19-tracker/static/css/main.15f3febd.chunk.css"
+  },
+  {
+    "revision": "c8c71721e92fa31cec59",
+    "url": "/covid19-tracker/static/js/2.abe17600.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.abe17600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743dd6d023bf18297349",
+    "url": "/covid19-tracker/static/js/main.d8ab3ebb.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/image.d7265326.png"
+  }
+]);
